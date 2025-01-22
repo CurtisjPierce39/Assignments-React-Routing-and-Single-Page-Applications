26e025b29f8bac889298c4e3d5dc397efc3d619f
Curtis J Pierce <Curtisjpierce39@gmail.com>
2025-01-22 13:38:31 -0800

    add initial description

--- comic-book-router/src/components/CharacterDetails.jsx
+++ comic-book-router/src/components/CharacterDetails.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
@@ -6,11 +6,13 @@ import axios from "axios";
 export const Marvel = () => {
     const { id } = useParams();
     const [item, setItems] = useState()
+    useEffect(()=>{
     const fetch = async () => {
         const res = await axios.get(`https://gateway.marvel.com:443/v1/public/characters/${id}?ts=1&apikey=d0137fa40d5132a1d5e0eb148caa0932&hash=b4b87f94c2284ac8230ef8908d164ee8`)
         setItems(res.data.data.results[0])
     }
     fetch();
+})
     return (
         <>
             {
--- comic-book-router/src/components/comics.jsx
+++ comic-book-router/src/components/comics.jsx
@@ -7,7 +7,7 @@ export const Comics = () => {
     const { id } = useParams();
     const [item, setItems] = useState()
     const fetch = async () => {
-        const res = await axios.get(`https://gateway.marvel.com:443/v1/public/comics?ts=1&apikey=d0137fa40d5132a1d5e0eb148caa0932&hash=b4b87f94c2284ac8230ef8908d164ee8`)
+        const res = await axios.get(`https://gateway.marvel.com:443/v1/public/characters/${id}?ts=1&apikey=d0137fa40d5132a1d5e0eb148caa0932&hash=b4b87f94c2284ac8230ef8908d164ee8`)
         setItems(res.data.data.results[0])
     }
     fetch();
@@ -16,9 +16,9 @@ export const Comics = () => {
             {
                 (!item) ? "" : (
                     <div className="box-content">
-                        {/* <div className="right-box">
+                        <div className="right-box">
                             <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt="hero image" />
-                        </div> */}
+                        </div>
                         <div className="left-box">
                             <h1>Comics</h1>
                             <ul>
